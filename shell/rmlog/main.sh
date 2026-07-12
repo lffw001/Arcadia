78b814ad7ac470939d0a4b268fd1750e379b2f56
Super Manito <68613938+SuperManito@users.noreply.github.com>
2026-07-13 05:52:42 +0800

    feat(cli): 完善消息中心CLI调用封装

--- shell/rmlog/main.sh
+++ shell/rmlog/main.sh
@@ -51,6 +51,16 @@ function command_rmlog_main() {
         fi
     }
 
+    ## 清理消息中心已读消息
+    function rm_message() {
+        local data='{"days": '"${RmDays}"'}'
+        local res=$(curl -s -X POST -H "Content-Type: application/json" -d "${data}" "http://127.0.0.1:5678/api/inner/message/clean")
+        local count="$(echo "${res}" | jq -rc '.data.count')"
+        if [[ "${count}" -gt 0 ]] 2>/dev/null; then
+            echo -e "已清理 ${BLUE}${count}${PLAIN} 条已读消息"
+        fi
+    }
+
     case $# in
     0)
         import_config_not_check
@@ -71,6 +81,7 @@ function command_rmlog_main() {
         [ -d "$ConfigDir/bak" ] && rm_empty_dir "$ConfigDir/bak" # 删除备份配置文件目录下的空文件夹
         [ -f $RootDir/core ] && rm -rf $RootDir/core             # 删除缓存
         rm_sys_log                                               # 清理后端系统日志
+        rm_message                                               # 清理消息中心已读消息
         echo -e "\n$COMPLETE 运行结束\n"
     fi
     cd $current_dir
